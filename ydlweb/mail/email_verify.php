2f4f0467408cb9c743f578753712919f3fb69372
WenYan1 <754883113@qq.com>
2019-01-21 11:48:04 +0800

    更改邮件发送端口

--- ydlweb/mail/email_verify.php
+++ ydlweb/mail/email_verify.php
@@ -4,7 +4,7 @@
 <p style="font-size: 14px;">您好:</p>
 <p style="font-size: 14px; text-indent: 28px; "><?= $email; ?> 在腾邦易贸通创建了帐号,为了保证您的正常使用,请点击下边的链接进行验证:</p>
 <!--<p style="color: #4E99B8; font-size: 12px">http://107.170.254.164/user-verify?token=<?= $verifyCode ?></p>-->
-<p style="color: #4E99B8; font-size: 12px">http://ymt.tempushold.com:77/user-verify?token=<?= $verifyCode ?></p>
+<p style="color: #4E99B8; font-size: 12px">http://ymt.tempushold.com:8889/user-verify?token=<?= $verifyCode ?></p>
 <p style="font-size: 14px;  color: #666666; text-indent: 28px;">您也可以将链接复制到浏览器中打开.</p>
 <hr style="border-top: 1px  #d8d8d8">
 <p align="center" style="text-align: center; font-size: 12px; color: #666666";>感谢您对 腾邦易贸通 的支持,我们将会为您提供最贴心的服务</p>
--- ydlweb/mail/forget_password.php
+++ ydlweb/mail/forget_password.php
@@ -3,7 +3,7 @@
 <hr style="border-top: 1px  #d8d8d8">
 <p style="font-size: 14px;">点击下方连接重置 腾邦易贸通 密码:</p>
 <!--<p style="color: #4E99B8; font-size: 12px">http://www.ydlweb.com/user-verify?token=<?=$verifyCode?></p>-->
-<p style="color: #4E99B8; font-size: 12px">http://ymt.tempushold.com:77/user-verify?token=<?=$verifyCode?></p>
+<p style="color: #4E99B8; font-size: 12px">http://ymt.tempushold.com:8889/user-verify?token=<?=$verifyCode?></p>
 <p style="font-size: 14px;  color: #666666;">如果您没有要求重置密码,请忽略此邮件.</p>
 <hr style="border-top: 1px  #d8d8d8">
 <p align="center" style="text-align: center; font-size: 12px; color: #666666";>感谢您对 腾邦易贸通的支持,我们将会为您提供最贴心的服务</p>
